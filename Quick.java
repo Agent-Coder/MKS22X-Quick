d0bd3d6d80f01069a4fe2a5897ea4f1af0ea6fbe
Amanda Zheng <azheng00@stuy.edu>
2019-03-11 19:15:53 -0400

    partition for start end works

--- Quick.java
+++ Quick.java
@@ -3,66 +3,63 @@ public class Quick{
   public static int partition(int[] data,int start, int end){
     int pos=start;
     int x=(int)(Math.random()*100%(end-start+1))+start;
-    //System.out.println(x);
-    //holds index of chosen pivot point
+    System.out.println(x);
+  //holds index of chosen pivot point
     int temp=data[start];
-    //temporary used during switching
+  //temporary used during switching
     data[start]=data[x];
     data[x]=temp;
-    //switch values to make pivot first one
-    for (int i=start+1;i<end+1;i++ ) {
-      /*for (int h=0;h<data.length ;h++ ) {
-        System.out.print(data[h]+" ");
-      }
-      System.out.println(" ");*/
-      if (data[i]<data[start]){
-        pos++;
-        temp=data[pos];
-        data[pos]=data[i];
-        data[i]=temp;
+  //switch values to make pivot first one
+  for (int i=start+1;i<end+1;i++) {
+    for (int h=0;h<data.length;h++ ) {
+      System.out.print(data[h]+" ");
     }
-    //if the number is less than pivot, increase the position and then switch
+    System.out.println("");
+    if (data[i]<data[start]){
+      pos++;
+      temp=data[pos];
+      data[pos]=data[i];
+      data[i]=temp;
   }
-  /*for (int h=0;h<data.length ;h++ ) {
-    System.out.print(data[h]+" ");
-  }
-  System.out.println(" ");*/
-  temp=data[start];
-  data[start]=data[pos];
-  data[pos]=temp;
-  //switch pivot point to the pos
-  /*for (int h=0;h<data.length ;h++ ) {
-    System.out.print(data[h]+" ");
-  }
-  System.out.println(" ");*/
-  return pos;
+  //if the number is less than pivot, increase the position and then switch
 }
+temp=data[start];
+data[start]=data[pos];
+data[pos]=temp;
+//switch pivot point to the pos
+for (int h=0;h<data.length ;h++ ) {
+  System.out.print(data[h]+" ");
+}
+System.out.println(" ");
+return pos;
+    }
 public static String toString(int[] data){
   String s="";
   for (int x=0;x<data.length;x++){
     s+=x+",";
   }
-  return s;
+  return s+"\n";
 }
 //return value that is the kth smallest value of the array
 public static int quickselect(int[] data,int k){
-
-  int temp=partition(data, 0,data.length-1);
-
-  while(temp!=data.length-k){
-      System.out.println("i");
-    if(temp>k){
-      temp=partition(data, temp,data.length-1);
-    }
-    if(temp<k){
-      temp=partition(data, 0,temp);
-    }
-  }
-  return data[temp];
+  return selecting(data,k,0,data.length-1,0);
 }
-  public static void main(String[] args) {
+public static int selecting(int[] data, int k, int start,int end,int current){
+  current=partition(data,start,end);
+
+  if(current>k){
+    return selecting(data,k,0,current,current);
+  }
+  if(current<k){
+    return selecting(data,k,current,data.length-1,current);
+  }
+  return data[current];
+}
+ public static void main(String[] args) {
 
     int[] ary= { 2, 10, 15, 23, 0,  5} ;
+    System.out.println(partition(ary,3,5));
+    /*System.out.println(ary);
     quickselect( ary , 0 );
     System.out.println(toString(ary));
     System.out.println("would return 0");
@@ -80,6 +77,6 @@ public static int quickselect(int[] data,int k){
     System.out.println("would return 15");
     quickselect( ary , 5 )  ;
     System.out.println(toString(ary));
-    System.out.println("would return 23");
+    System.out.println("would return 23");*/
   }
 }
