e46009a63f46e6a24fa1778e6d28744fb38847a5
Amanda Zheng <azheng00@stuy.edu>
2019-03-12 12:37:52 -0400

    everything works according to Theodore's Driver

--- Quick.java
+++ Quick.java
@@ -1,8 +1,9 @@
 import java.lang.Math;
 public class Quick{
   public static int partition(int[] data,int start, int end){
+    boolean rightside=false;
     int pos=start;
-    int x=(int)(Math.random()*data.length%(end-start+1))+start;
+    int x=median(data,start,end,((end-start)/2)+start);
     //System.out.println(x);
   //holds index of chosen pivot point
     int temp=data[start];
@@ -15,17 +16,24 @@ public class Quick{
     System.out.println(i);
     System.out.println("pos" + pos);
     System.out.println(start);
-    System.out.println(end);
-    for (int h=0;h<data.length;h++ ) {
+    System.out.println(end);*/
+    /*for (int h=0;h<data.length;h++ ) {
       System.out.print(data[h]+" ");
     }
     System.out.println("");*/
+    rightside=!rightside;
     if (data[i]<data[start]){
       pos++;
       temp=data[pos];
       data[pos]=data[i];
       data[i]=temp;
-  }
+    }
+    if(data[i]==data[start]&&Math.random()*100%2==0){
+      pos++;
+      temp=data[pos];
+      data[pos]=data[i];
+      data[i]=temp;
+    }
   //if the number is less than pivot, increase the position and then switch
 }
 temp=data[start];
@@ -47,6 +55,17 @@ return pos;
   return s+"\n";
 }*/
 //return value that is the kth smallest value of the array
+public static int median(int data[],int low,int high, int middle){
+  if(data[low]>=data[high]&&data[low]<=data[middle]){
+    return low;
+  }
+  else if(data[low]<=data[high]&&data[high]<=data[middle]){
+    return high;
+  }
+  else{
+    return middle;
+  }
+}
 public static int quickselect(int[] data,int k){
   return selecting(data,k,0,data.length-1);
 }
@@ -63,19 +82,19 @@ public static int selecting(int[] data, int k, int start,int end){
     System.out.println("");
 */
     if(current>k){
-      end=current;
+      end=current-1;
     }
     if(current<k){
-      start=current;
+      start=current+1;
     }
   }
   return data[current];
 }
-/* public static void main(String[] args) {
-   int[] ary= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18} ;
-    //int[] ary= { 2, 22,33,22,33,7} ;
+ public static void main(String[] args) {
+   //int[] ary= {1} ;
+    int[] ary= { 2, 22,33,22,33,7} ;
 
-    //System.out.println(partition( ary , 0,5 ));
+    //System.out.println(partition( ary ,0,5 ));
     //System.out.println(toString(ary));
     //System.out.println("would return 2");
     //System.out.println(quickselect( ary , 2 )) ;
@@ -84,11 +103,11 @@ public static int selecting(int[] data, int k, int start,int end){
     //System.out.println(quickselect( ary , 3 ))  ;
     //System.out.println(toString(ary));
     //System.out.println("would return 10");
-    System.out.println(quickselect( ary , 4 ));
+    //System.out.println(quickselect( ary , 0 ));
     //System.out.println(toString(ary));
-    System.out.println("would return 15");
+    /*System.out.println("would return 15");
     System.out.println(quickselect( ary , 5 ));
     //System.out.println(toString(ary));
-    System.out.println("would return 23");
-  }*/
+    System.out.println("would return 23");*/
+  }
 }
